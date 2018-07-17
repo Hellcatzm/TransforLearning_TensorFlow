efac87a608ddf9a79d268d5e50ea9733712c1f3b
叠加态的猫 <18811474740@163.com>
2018-07-17 19:14:41 +0800

    预测多张图片

--- TransferLearning_reload.py
+++ TransferLearning_reload.py
@@ -18,7 +18,8 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 
-image_path = '/home/hellcat/PycharmProjects/CV&DL/迁移学习/flower_photos/daisy/5673551_01d1ea993e_n.jpg'
+image_path = ['/home/hellcat/PycharmProjects/CV&DL/迁移学习/flower_photos/daisy/5673551_01d1ea993e_n.jpg',
+              '/home/hellcat/PycharmProjects/CV&DL/迁移学习/flower_photos/roses/99383371_37a5ac12a3_n.jpg']
 
 # with open(os.path.join(MODEL_DIR, MODEL_FILE), 'rb') as f:  # 阅读器上下文
 #     graph_def = tf.GraphDef()  # 生成图
@@ -35,14 +36,14 @@ saver = tf.train.import_meta_graph(ckpt.model_checkpoint_path + '.meta')
 saver.restore(sess, ckpt.model_checkpoint_path)
 # pp.pprint(tf.get_default_graph().get_operations())
 g = tf.get_default_graph()
+for img in image_path:
+    image_data = open(img, 'rb').read()
+    bottleneck = sess.run(g.get_tensor_by_name('import/pool_3/_reshape:0'),
+                          feed_dict={g.get_tensor_by_name('import/DecodeJpeg/contents:0'): image_data})
 
-image_data = open(image_path, 'rb').read()
-bottleneck = sess.run(g.get_tensor_by_name('import/pool_3/_reshape:0'),
-                      feed_dict={g.get_tensor_by_name('import/DecodeJpeg/contents:0'): image_data})
+    class_result = sess.run(g.get_tensor_by_name('final_train_ops/Softmax:0'),
+                            feed_dict={g.get_tensor_by_name('BottleneckInputPlaceholder:0'): bottleneck})
 
-class_result = sess.run(g.get_tensor_by_name('final_train_ops/Softmax:0'),
-                        feed_dict={g.get_tensor_by_name('BottleneckInputPlaceholder:0'): bottleneck})
-
-images_lists = creat_image_lists(10, 10)
-tf.logging.info(images_lists.keys())
-print(np.squeeze(class_result))
+    images_lists = creat_image_lists(10, 10)
+    tf.logging.info(images_lists.keys())
+    print(np.squeeze(class_result))
